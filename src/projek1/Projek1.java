e452cfcbee302a55b5db06762f0e1b1fa6d20759
helmyrahmat <helmyrahmat5@gmail.com>
2025-03-26 10:13:22 +0700

    perubahan

---
+++ src/projek1/Projek1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projek1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Projek1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int data [][] ={{4,6,4,2,8,4,2,10},{4,6,4,2,8,4,2,10}};
+        for (int a = 0; a < data.length; a++){
+            for ( int b = 0; b < data[0].length; b++){
+                System.out.print(data [a][b]+ " ");
+            }
+            System.out.print(" \n") ;
+        
+      }
+    }
+    
+}
